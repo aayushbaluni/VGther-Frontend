98a948ef3d1e2aed333c780dc4a3c812e9391592
Ayush Baluni <73417844+aayushbaluni@users.noreply.github.com>
2023-05-14 11:38:19 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
        <Box minH={['0','calc(40vh)']} display={'flex'} flexDir={'column'} w={['100%','50%']}>
        <Heading color={'white'} fontSize={'6vh'}>V-Gthr</Heading>
         <Text marginBottom={'10'} color={'white'} >We Gather We Wibe Together</Text>
-      <Link href='https://merchant.razorpay.com/policy/Lp7kCx8I3hWJRn/privacy' isExternal ><Button variant={'link'} color={'white'}>Privacy Policy</Button></Link>
-      <Link href='https://merchant.razorpay.com/policy/Lp7kCx8I3hWJRn/terms' isExternal ><Button variant={'link'} color={'white'}>Terms and Conditions</Button></Link>
+      <Link href='https://merchant.razorpay.com/policy/LpQvwLwV81sPcC/privacy' isExternal ><Button variant={'link'} color={'white'}>Privacy Policy</Button></Link>
+      <Link href='https://merchant.razorpay.com/policy/LpQvwLwV81sPcC/terms' isExternal ><Button variant={'link'} color={'white'}>Terms and Conditions</Button></Link>
       
        </Box>
       <Box w={['100%','50%']} minH={['calc(10vh)','calc(40vh)']} alignItems={'center'} display={'flex'} flexDir={'column'}></Box>
